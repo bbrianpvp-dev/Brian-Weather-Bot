9e110bcc2c65f4f66ac9378ebfb84422d4b0aeac
BrianPVP <bbrianpvp@gmail.com>
2026-06-27 17:32:11 +0800

    Add earthquake module structure

---
+++ src/earthquake.py
@@ -0,0 +1,10 @@
+def get_earthquake():
+
+    earthquake = {
+        "time": "07:31",
+        "magnitude": 5.4,
+        "depth": 32,
+        "location": "花蓮外海"
+    }
+
+    return earthquake
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from weather import get_weather
+from earthquake import get_earthquake
 
 
 print("🌤️ Brian Weather Bot")
@@ -13,3 +14,14 @@ print("☁️ 天氣:", weather["weather"])
 print("☔ 降雨機率:", weather["rain"], "%")
 print("🌡️ 最低溫:", weather["min_temp"], "°C")
 print("🌡️ 最高溫:", weather["max_temp"], "°C")
+
+earthquake = get_earthquake()
+
+print()
+
+print("🚨 地震速報")
+
+print("時間:", earthquake["time"])
+print("規模:", earthquake["magnitude"])
+print("深度:", earthquake["depth"], "km")
+print("位置:", earthquake["location"])
