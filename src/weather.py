e4c24976d852c55c7c30375b51c530e28497e997
BrianPVP <bbrianpvp@gmail.com>
2026-06-27 18:33:21 +0800

    Fix weather temperature parsing

--- src/weather.py
+++ src/weather.py
@@ -4,7 +4,7 @@ from config import API_KEY
 
 def get_weather():
 
-    url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-063"
+    url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
 
     params = {
         "Authorization": API_KEY,
@@ -13,45 +13,61 @@ def get_weather():
     }
 
 
-    response = requests.get(url, params=params)
+    response = requests.get(
+        url,
+        params=params
+    )
+
 
     data = response.json()
 
 
-    location = data["records"]["Locations"][0]["Location"][0]
+    location = data["records"]["location"][0]
 
 
-    weather_elements = location["WeatherElement"]
+    result = {}
+
+    result["location"] = location["locationName"]
 
 
-    result = {
-        "location": "臺北市",
-        "weather": "",
-        "rain": "",
-        "min_temp": "",
-        "max_temp": ""
-    }
+    weather = location["weatherElement"]
 
 
-    for element in weather_elements:
+    for element in weather:
 
-        name = element["ElementName"]
-
-
-        if name == "天氣現象":
-            result["weather"] = element["Time"][0]["ElementValue"][0]["Weather"]
+        name = element["elementName"]
 
 
-        elif name == "降雨機率":
-            result["rain"] = element["Time"][0]["ElementValue"][0]["ProbabilityOfPrecipitation"]
+        if name == "Wx":
+            result["weather"] = (
+                element["time"][0]
+                ["parameter"]
+                ["parameterName"]
+            )
 
 
-        elif name == "最低溫度":
-            result["min_temp"] = element["Time"][0]["ElementValue"][0]["MinTemperature"]
+        elif name == "PoP":
+            result["rain"] = (
+                element["time"][0]
+                ["parameter"]
+                ["parameterName"]
+            )
 
 
-        elif name == "最高溫度":
-            result["max_temp"] = element["Time"][0]["ElementValue"][0]["MaxTemperature"]
+        elif name == "MinT":
+            result["min_temp"] = (
+                element["time"][0]
+                ["parameter"]
+                ["parameterName"]
+            )
+
+
+        elif name == "MaxT":
+            result["max_temp"] = (
+                element["time"][0]
+                ["parameter"]
+                ["parameterName"]
+            )
 
 
     return result
